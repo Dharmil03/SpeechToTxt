a6c356b29104f5bb59abb3f0153d494aefd2b8a3
Dharmil Shah <shahdharmil3103@gmail.com>
2024-05-01 14:02:43 +0530

    app set up done

---
+++ README.md
@@ -0,0 +1,21 @@
+# SpeechTXT
+
+SpeechTXT is a responsive speech-to-text converter application developed using React and the React Speech Recognition library. It allows users to convert speech into text with ease.
+
+## Features
+
+- **Responsive Design:** Accessible on various devices, ensuring a seamless user experience.
+- **Pause & Resume:** Users can pause and resume the speech recognition process at their convenience.
+- **Download Transcripts:** Easily download the converted text transcripts as TXT files.
+- **Reset Anytime:** Start fresh by resetting the transcript whenever needed.
+
+## Demo
+
+Check out the live demo [here](https://speech-txt-neon.vercel.app/).
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/your-username/speech-txt.git
---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import "./App.css";
+import { IoCloudDownloadOutline } from "react-icons/io5";
+import { TiMicrophoneOutline } from "react-icons/ti";
+import { IoStopCircleOutline } from "react-icons/io5";
+import { MdLockReset } from "react-icons/md";
+import 'regenerator-runtime/runtime'
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+function App() {
+
+  const startListening= ()=>SpeechRecognition.startListening({ continuous: true , language:"en-IN"})
+  const {
+    transcript,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+  const downloadTranscript = () => {
+    const element = document.createElement('a');
+    const file = new Blob([transcript], { type: 'text/plain' });
+    element.href = URL.createObjectURL(file);
+    element.download = 'transcript.txt';
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  };
+  return (
+    <>
+      <h1 className="heading">Speech to Text </h1>
+      <div className="input_box">
+        <p className="input_box--text">{transcript}</p>
+      </div>
+      <div className="card">
+        <button onClick={startListening} area-label="start Listening">
+          <TiMicrophoneOutline size="20px" />
+        </button>
+        <button onClick={SpeechRecognition.stopListening} area-label="stop listening">
+          <IoStopCircleOutline size="20px" />
+        </button>
+        <button onClick={downloadTranscript} area-label="download transcript">
+          <IoCloudDownloadOutline size="20px" />
+        </button>
+        <button onClick={resetTranscript} area-label="reset transcript">
+          <MdLockReset size="20px" />
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
